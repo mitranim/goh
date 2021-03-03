c1b3ad8d0bd92def9ff553ad2380e045f8654437
Nelo Mitranim <me@mitranim.com>
2021-03-03 16:22:41 +0300

    init

---
+++ goh.go
@@ -0,0 +1,330 @@
+/*
+goh = Go Http Handlers.
+
+Utility types that represent a not-yet-sent HTTP response as a value
+(status, header, body) with NO added abstractions or interfaces. All types
+implement `http.Hander`.
+
+See `readme.md` for examples.
+*/
+package goh
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+/*
+Error handler function provided by user code to the various `http.Handler` types
+in this library, such as `String`.
+
+When nil, `ErrHandlerDefault` is used.
+
+The `wrote` parameter indicates whether the response writer has been written to.
+If `false`, the handler should write an error response. If `true`, or if
+sending the error response has failed, the handler should log the resulting
+error to the server log.
+*/
+type ErrFunc = func(rew http.ResponseWriter, req *http.Request, wrote bool, err error)
+
+/*
+Default error handler, used by `http.Handler` types when no `ErrFunc` was
+provided. May be overridden globally.
+*/
+var ErrHandlerDefault = ErrHandler
+
+/*
+Default implementation of `ErrFunc`. Used by `http.Handler` types, such as
+`String`, when no `ErrFunc` was provided by user code.
+
+If possible, writes the error to the response writer as plain text. If not, logs
+the error to the standard error stream.
+
+When implementing a custom error handler, use this function's source as an
+example.
+*/
+func ErrHandler(rew http.ResponseWriter, req *http.Request, wrote bool, err error) {
+	if err == nil {
+		return
+	}
+
+	if !wrote {
+		rew.WriteHeader(http.StatusInternalServerError)
+		_, err = fmt.Fprint(rew, err)
+		if err != nil {
+			// Logged below.
+			err = fmt.Errorf(`secondary error while writing error response: %w`, err)
+		}
+	}
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%+v\n", err)
+	}
+}
+
+/*
+The head part of each `http.Handler` implementation in this package.
+Pseudo-embedded in the various handler types, obtained via `.Head()` methods.
+
+We're not using actual embedding because Go would require literal constructors
+of handler types to contain `Head: Head{}` for promoted fields.
+
+See `Reader`, `Bytes`, `String`, `Json`, `Xml`.
+*/
+type Head struct {
+	Status  int
+	Header  http.Header
+	ErrFunc ErrFunc
+}
+
+/*
+Writes the header and HTTP status (if any) to the provided writer. Called
+internally by the various handler types. You shouldn't have to call this
+yourself, unless implementing a new type.
+
+This must be called exactly once, and only before writing the body.
+*/
+func (self Head) Write(rew http.ResponseWriter) {
+	target := rew.Header()
+
+	for key, vals := range self.Header {
+		target.Del(key)
+		for _, val := range vals {
+			target.Add(key, val)
+		}
+	}
+
+	if self.Status > 0 {
+		rew.WriteHeader(self.Status)
+	}
+}
+
+func (self Head) handleErr(rew http.ResponseWriter, req *http.Request, wrote bool, err error) {
+	if err == nil {
+		return
+	}
+	if self.ErrFunc == nil {
+		self.ErrFunc = ErrHandlerDefault
+	}
+	self.ErrFunc(rew, req, wrote, err)
+}
+
+/*
+HTTP handler that copies a response from a reader.
+
+Caution: if the reader is also `io.Closer`, it must be closed in your code.
+This package does NOT attempt to do that.
+*/
+type Reader struct {
+	Status  int
+	Header  http.Header
+	Body    io.Reader
+	ErrFunc ErrFunc
+}
+
+var _ = http.Handler(Reader{})
+
+// Implement `http.Handler`.
+func (self Reader) ServeHTTP(rew http.ResponseWriter, req *http.Request) {
+	self.Head().Write(rew)
+
+	if self.Body != nil {
+		_, err := io.Copy(rew, self.Body)
+		if err != nil {
+			err = fmt.Errorf(`failed to write response: %w`, err)
+			self.Head().handleErr(rew, req, true, err)
+		}
+	}
+}
+
+// Returns the pseudo-embedded `Head` part.
+func (self Reader) Head() Head { return Head{self.Status, self.Header, self.ErrFunc} }
+
+/*
+HTTP handler that writes bytes. Note: for sending a string, use `String`, which
+should be marginally more efficient.
+*/
+type Bytes struct {
+	Status  int
+	Header  http.Header
+	Body    []byte
+	ErrFunc ErrFunc
+}
+
+var _ = http.Handler(Bytes{})
+
+// Implement `http.Handler`.
+func (self Bytes) ServeHTTP(rew http.ResponseWriter, req *http.Request) {
+	self.Head().Write(rew)
+
+	_, err := rew.Write(self.Body)
+	if err != nil {
+		err = fmt.Errorf(`failed to write response: %w`, err)
+		self.Head().handleErr(rew, req, true, err)
+	}
+}
+
+// Returns the pseudo-embedded `Head` part.
+func (self Bytes) Head() Head { return Head{self.Status, self.Header, self.ErrFunc} }
+
+// Shortcut for `BytesWith(http.StatusOK, body)`.
+func BytesOk(body []byte) Bytes {
+	return BytesWith(http.StatusOK, body)
+}
+
+// Shortcut for `Bytes` with specific status and body.
+func BytesWith(status int, body []byte) Bytes {
+	return Bytes{Status: status, Body: body}
+}
+
+/*
+HTTP handler that writes a string. Note: for sending bytes, use `Bytes`, which
+should be marginally more efficient.
+*/
+type String struct {
+	Status  int
+	Header  http.Header
+	Body    string
+	ErrFunc ErrFunc
+}
+
+var _ = http.Handler(String{})
+
+// Implement `http.Handler`.
+func (self String) ServeHTTP(rew http.ResponseWriter, req *http.Request) {
+	self.Head().Write(rew)
+
+	_, err := io.WriteString(rew, self.Body)
+	if err != nil {
+		err = fmt.Errorf(`failed to write response: %w`, err)
+		self.Head().handleErr(rew, req, true, err)
+	}
+}
+
+// Returns the pseudo-embedded `Head` part.
+func (self String) Head() Head { return Head{self.Status, self.Header, self.ErrFunc} }
+
+// Shortcut for `StringWith(http.StatusOK, body)`.
+func StringOk(body string) String {
+	return StringWith(http.StatusOK, body)
+}
+
+// Shortcut for `String` with specific status and body.
+func StringWith(status int, body string) String {
+	return String{Status: status, Body: body}
+}
+
+/*
+HTTP handler that automatically sets the appropriate JSON headers and encodes
+its body as JSON. Currently does not support custom encoder options; if you
+need that feature, open an issue or PR.
+*/
+type Json struct {
+	Status  int
+	Header  http.Header
+	Body    interface{}
+	ErrFunc ErrFunc
+}
+
+var _ = http.Handler(Json{})
+
+// Implement `http.Handler`.
+func (self Json) ServeHTTP(rew http.ResponseWriter, req *http.Request) {
+	rew.Header().Set("Content-Type", "application/json")
+	self.Head().Write(rew)
+
+	writer := spyingWriter{Writer: rew}
+	err := json.NewEncoder(&writer).Encode(self.Body)
+	if err != nil {
+		err = fmt.Errorf(`failed to write response as JSON: %w`, err)
+		self.Head().handleErr(rew, req, writer.wrote, err)
+	}
+}
+
+// Returns the pseudo-embedded `Head` part.
+func (self Json) Head() Head { return Head{self.Status, self.Header, self.ErrFunc} }
+
+// Shortcut for `JsonWith(http.StatusOK, body)`.
+func JsonOk(body interface{}) Json {
+	return JsonWith(http.StatusOK, body)
+}
+
+// Shortcut for `Json` with specific status and body.
+func JsonWith(status int, body interface{}) Json {
+	return Json{Status: status, Body: body}
+}
+
+/*
+HTTP handler that automatically sets the appropriate XML headers and encodes
+its body as XML. Currently does not support custom encoder options; if you
+need that feature, open an issue or PR.
+*/
+type Xml Json
+
+var _ = http.Handler(Xml{})
+
+// Implement `http.Handler`.
+func (self Xml) ServeHTTP(rew http.ResponseWriter, req *http.Request) {
+	rew.Header().Set("Content-Type", "application/xml")
+	self.Head().Write(rew)
+
+	writer := spyingWriter{Writer: rew}
+	err := xml.NewEncoder(&writer).Encode(self.Body)
+	if err != nil {
+		err = fmt.Errorf(`failed to write response as XML: %w`, err)
+		self.Head().handleErr(rew, req, writer.wrote, err)
+	}
+}
+
+// Returns the pseudo-embedded `Head` part.
+func (self Xml) Head() Head { return Head{self.Status, self.Header, self.ErrFunc} }
+
+// Shortcut for `XmlWith(http.StatusOK, body)`.
+func XmlOk(body interface{}) Xml {
+	return XmlWith(http.StatusOK, body)
+}
+
+// Shortcut for `Xml` with specific status and body.
+func XmlWith(status int, body interface{}) Xml {
+	return Xml{Status: status, Body: body}
+}
+
+/*
+HTTP handler that performs an HTTP redirect.
+*/
+type Redirect struct {
+	Status  int
+	Header  http.Header
+	Link    string
+	ErrFunc ErrFunc
+}
+
+var _ = http.Handler(Redirect{})
+
+// Implement `http.Handler`.
+func (self Redirect) ServeHTTP(rew http.ResponseWriter, req *http.Request) {
+	self.Head().Write(rew)
+	http.Redirect(rew, req, self.Link, self.Status)
+}
+
+// Returns the pseudo-embedded `Head` part.
+func (self Redirect) Head() Head { return Head{self.Status, self.Header, self.ErrFunc} }
+
+// Shortcut for `Redirect` with specific status and body.
+func RedirectWith(status int, link string) Redirect {
+	return Redirect{Status: status, Link: link}
+}
+
+type spyingWriter struct {
+	io.Writer
+	wrote bool
+}
+
+func (self *spyingWriter) Write(chunk []byte) (int, error) {
+	self.wrote = true
+	return self.Writer.Write(chunk)
+}
---
+++ readme.md
@@ -0,0 +1,91 @@
+## Overview
+
+`goh` = **Go** **H**TTP **H**andlers.
+
+Utility types that represent a not-yet-sent HTTP response as a value (status, header, body) with _no added abstractions or interfaces_. All types implement `http.Hander`.
+
+See API docs at https://pkg.go.dev/github.com/mitranim/goh.
+
+## Usage
+
+Simple example, relying on default error handler:
+
+```golang
+import "github.com/mitranim/goh"
+
+// Simple example. Relies on default error handler.
+func handler(rew http.ResponseWriter, req *http.Request) {
+  res := goh.StringOk("response text")
+  res.ServeHTTP(rew, req)
+}
+
+// Simple example with status and headers. Relies on default error handler.
+func handler(rew http.ResponseWriter, req *http.Request) {
+  res := goh.String{
+    Status: http.StatusCreated,
+    Header: http.Header{"Content-Type": {"text/html"}},
+    Body:   "<body>response text</body>",
+  }
+  res.ServeHTTP(rew, req)
+}
+
+// Simple JSON example. Relies on default error handler.
+func handler(rew http.ResponseWriter, req *http.Request) {
+  res := goh.JsonOk(Val{10, 20})
+  res.ServeHTTP(rew, req)
+}
+
+// Example with custom error handler.
+func handler(rew http.ResponseWriter, req *http.Request) {
+  res := goh.Json{
+    Body: Val{10, 20},
+    ErrFunc: writeErrAsJson,
+  }
+  res.ServeHTTP(rew, req)
+}
+
+// Example with custom error handler.
+// Should be provided to response types as `ErrFunc: writeErrAsJson`.
+func writeErrAsJson(
+  rew http.ResponseWriter, req *http.Request, wrote bool, err error,
+) {
+  if err == nil {
+    return
+  }
+
+  if !wrote {
+    rew.WriteHeader(http.StatusInternalServerError)
+    err = json.NewEncoder(rew).Encode(ErrJson{err})
+    if err != nil {
+      // Logged below.
+      err = fmt.Errorf(`secondary error while writing error response: %w`, err)
+    }
+  }
+
+  if err != nil {
+    fmt.Fprintf(os.Stderr, "%+v\n", err)
+  }
+}
+
+// Can replace the default error handler globally. Don't abuse this power.
+func init() {
+  goh.ErrHandlerDefault = writeErrAsJson
+}
+
+type Val struct {
+  One int64 `json:"one"`
+  Two int64 `json:"two"`
+}
+
+type ErrJson struct {
+  Error error `json:"error"`
+}
+```
+
+## License
+
+https://unlicense.org
+
+## Misc
+
+I'm receptive to suggestions. If this library _almost_ satisfies you but needs changes, open an issue or chat me up. Contacts: https://mitranim.com/#contacts
